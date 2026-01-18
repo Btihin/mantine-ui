7c998d16583156e33c952caec2b91c90a6b5882d
Btihin <tomas.trnka@fs.gov.cz>
2026-01-18 19:50:26 +0100

    try timepout

--- src/pages/shared/HomePage.tsx
+++ src/pages/shared/HomePage.tsx
@@ -1,7 +1,17 @@
 import { Button } from '@mantine/core';
 import { notifications } from '@mantine/notifications';
+import { useEffect, useState } from 'react';
 
 const HomePage = () => {
+  const [loading, setLoading] = useState(true);
+  const loadingPromise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Data načtena');
+    }, 6000);
+  });
+  useEffect(() => {
+    loadingPromise.then(() => setLoading(false));
+  }, []);
   return (
     <div>
       <h1>Template H1</h1>
@@ -14,6 +24,18 @@ const HomePage = () => {
         }
       >
         Show notification
+      </Button>
+      <Button
+        onClick={() =>
+          notifications.show({
+            title: 'Default notification',
+            message: 'Do not forget to star Mantine on GitHub! 🌟',
+            loading: loading,
+            autoClose: 8000,
+          })
+        }
+      >
+        druhá notifikace
       </Button>
     </div>
   );
